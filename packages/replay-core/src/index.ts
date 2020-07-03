2f0821b2b95f1c5215cc739d93f2f16b8eda6280
Ed Bentley <15923595+edbentley@users.noreply.github.com>
2020-07-03 10:56:54 +0100

    Add Native Sprites to core and web

--- packages/replay-core/src/index.ts
+++ packages/replay-core/src/index.ts
@@ -1,6 +1,12 @@
 // All of the public APIs used in games (follows SemVer)
 
 export { t, Texture, TextureFont } from "./t";
-export { Sprite, makeSprite } from "./sprite";
+export {
+  Sprite,
+  makeSprite,
+  makeNativeSprite,
+  NativeSpriteImplementation,
+  NativeSpriteUtils,
+} from "./sprite";
 export { Store, Device, DeviceSize } from "./device";
 export { GameProps, GameSize, GameOrientationSize } from "./core";
