891126d7e288dd37f79cb7259d9880b86537cb7d
Ed Bentley <15923595+edbentley@users.noreply.github.com>
2021-02-04 14:16:40 +0000

    Upgrade all packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,49 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { variables: false, functions: false },
     ],
-    "extends": ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-    "rules": {
-        "@typescript-eslint/no-use-before-define": ["error", { "variables": false, "functions": false }],
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
-    },
-    "overrides": [
-        {
-            "files": ["*.js"],
-            "rules": {
-                "@typescript-eslint/no-var-requires": "off",
-                "@typescript-eslint/no-unused-vars": "off"
-            }
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        types: {
+          "{}": false,
         },
-        {
-            "files": ["**/__tests__/*.ts"],
-            "rules": {
-                "@typescript-eslint/no-explicit-any": "off",
-                "@typescript-eslint/no-non-null-assertion": "off"
-            }
-        }
-    ]
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+      },
+    },
+    {
+      files: ["**/__tests__/*.ts"],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+      },
+    },
+  ],
 };
