4c9e13c3b86c125759b66dd8e9e66af5f33673e1
Ed Bentley <15923595+edbentley@users.noreply.github.com>
2021-11-11 11:09:18 +0000

    Android: auto hide status bars when they reappear (#101)

--- packages/replay-android/android-library/replay-android/src/main/java/com/replay/android/ReplayActivity.kt
+++ packages/replay-android/android-library/replay-android/src/main/java/com/replay/android/ReplayActivity.kt
@@ -108,8 +108,8 @@ open class ReplayActivity : AppCompatActivity() {
     }
 
     private fun hideSystemUI() {
-        // Enables regular immersive mode.
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+        // Enables sticky immersive mode.
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
